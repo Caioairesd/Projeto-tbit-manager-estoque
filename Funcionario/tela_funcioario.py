1956ab8aa2e51a3982315d4136d82ab7f327cee0
EberthSilva <eberthsilva011@gmail.com>
2025-03-06 22:13:50 -0300

    crud funcionário em andamento
    ajustando posições e etc

--- Funcionario/tela_funcioario.py
+++ Funcionario/tela_funcioario.py
@@ -1,27 +1,35 @@
 import tkinter as tk
 from tkinter import messagebox
 from crud_funcionario import fazer_login_funcionario
+from tkinter import*
+
 
 
 class CRUDfuncioario:
     def __init__(self, root):
         self.root = root 
         self.root.title ("CRUD FUNCIONARIO")
+        self.root.geometry ("900x700")
+        #self.root['bg'] = "MIDNIGHTBLUE"
+       
+
         
         
 
         self.criar_funcionario()
     def criar_funcionario(self):
-        tk.Label(self.root,text= "Nome: ").grid(row= 0, column= 1)
-        tk.Label(self.root,text= "Data de Nascimaento: ").grid(row= 1, column= 1)
-        tk.Label(self.root,text= "data de admisão: ").grid(row= 2, column= 1)
-        tk.Label(self.root,text= "CPF: ").grid(row= 3, column= 1)
-        tk.Label(self.root,text= "Cidade: ").grid(row= 4, column= 1)
-        tk.Label(self.root,text= "UF: ").grid(row= 5, column= 1)
-        tk.Label(self.root,text= "telefone: ").grid(row= 5, column= 1)
-        tk.Label(self.root,text= "Email: ").grid(row= 6, column= 1)
-        tk.Label(self.root,text= "Usuario: ").grid(row= 7, column= 1)
-        tk.Label(self.root,text= "Senha: ").grid(row= 8, column= 1)
+        tk.Label(self.root,text= "Nome: ").place(x= 0, y= 0)
+        tk.Label(self.root,text= "Data de Nascimento: ").place(x= 0, y= 20)
+        tk.Label(self.root,text= "Data de admissão: ").place(x= 0, y= 40)
+        tk.Label(self.root,text= "CPF: ").place(x= 0, y= 60)
+        tk.Label(self.root,text= "Cidade: ").place(x= 0, y= 80)
+        tk.Label(self.root,text= "Estado: ").place(x= 0, y= 100)
+        tk.Label(self.root,text= "Telefone: ").place(x= 0, y= 120)
+        tk.Label(self.root,text= "Email: ").place(x= 0, y= 140)
+        tk.Label(self.root,text= "Usuario: ").place(x= 0, y= 160)
+        tk.Label(self.root,text= "Senha: ").place(x= 0, y= 180)
+
+      
 
         self.nome_entry = tk.Entry(self.root)
         self.Data_de_nascimento_entry = tk.Entry(self.root)
@@ -35,7 +43,19 @@ class CRUDfuncioario:
         self.Usuario_entry = tk.Entry(self.root)
         self.Senha_entry = tk.Entry(self.root)
 
-        tk.Button(self.root, text= "Funcionario Login", command = self.fazer_login_funcionario).grid(row= 6, column= 0, columnspan= 1)
+        self.nome_entry.place(x= 50, y= 0)
+        self.Data_de_nascimento_entry.place(x= 50, y= 20)
+        self.Data_de_Admissao_entry.grid(row= 2, column= 10)
+        self.CPF_entry.grid(row= 3, column= 10)
+        self.Cidade_entry.grid(row= 4, column= 10)
+        self.UF_entry.grid(row= 5, column= 10)
+        self.Estado_entry.grid(row= 6, column= 10)
+        self.Telefone_entry.grid(row= 7, column= 10)
+        self.Email_entry.grid(row= 8, column= 10)
+        self.Usuario_entry.place(x= 100, y= 180)
+
+        tk.Button(self.root, text= "Funcionario Login", command = self.fazer_login_funcionario)
+        tk.Button.place(x= 200, y=290)
      
     def fazer_login_funcionario (self):
         nome = self.nome_entry.get()
