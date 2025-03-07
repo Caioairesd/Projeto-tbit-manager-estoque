e316bf9503e4db930c1de120dfbe0b2e88407ba0
Caio Vinicius <caioviniaires@gmail.com>
2025-03-06 22:12:48 -0300

    Códigos iniciais
    Códigos inicias Crud Fornecedor

---
+++ fornecedor/db_fornecedor.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from config_fornecedor import MYSQL_HOST,MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE 
+
+def get_connection():
+    return mysql.connector.connect(
+    host = MYSQL_HOST,
+    user = MYSQL_USER,
+    password = MYSQL_PASSWORD,
+    database = MYSQL_DATABASE)
+
+def create_fornecedor(nome_fornecedor,marca_fornecedor,email_fornecedor,telefone_fornecedor,cidade_fornecedor,pais_fornecedor):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "insert fornecedor(nome_fornecedor,marca_fornecedor,email_fornecedor,telefone_fornecedor,cidade_fornecedor,pais_fornecedor)VALUES(%s,%s,%s,%s,%s,%s,)"
+    cursor.execute(query,(nome_fornecedor,marca_fornecedor,email_fornecedor,telefone_fornecedor,cidade_fornecedor,pais_fornecedor))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
