147526657db24852448d5b9a7ee863d85010b408
baleradaniel <danielbalera021@gmail.com>
2025-03-07 21:59:06 -0300

    poor login screen

---
+++ tela_login/main_menu.py
@@ -0,0 +1,12 @@
+from tkinter import * 
+from tkinter import messagebox
+from tkinter import ttk 
+# from db import tbit_db
+
+janela = Tk()
+janela.title("TBit Manager                                                                                                                                                                                 by TerraBytes") 
+janela.geometry("900x700") 
+janela.configure(background="white") 
+janela.resizable(width=False, height=False) 
+
+janela.mainloop()
