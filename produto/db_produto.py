d8a5e034d25b985f8007a388abde5fe5344bf042
Kaio Mazza <mazza.kaio12@gmail.com>
2025-03-07 21:55:01 -0300

    Terminando cadastrar produto

--- produto/db_produto.py
+++ produto/db_produto.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from config_produto import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
 
-def get_connection(self):
+def get_connection():
     return mysql.connector.connect(
     host = MYSQL_HOST,
     user = MYSQL_USER,
@@ -18,3 +18,26 @@ def registrar_produto(nome, descricao, quantidade, valor):
     conn.commit()
     cursor.close()
     conn.close()
+
+def atualizar_produto(nome, descricao, quantidade, valor):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "UPDATE produto SET nome_produto = %s, descricao_produto = %s, quantidade_produto = %s, valor_produto = %s"
+    cursor.execute(query, (nome, descricao, quantidade, valor))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def pesquisar_produto(nome):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "SELECT %s FROM produto"
+    
+    busca = cursor.execute(query, (nome))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return busca
--- produto/tela_produto.py
+++ produto/tela_produto.py
@@ -2,7 +2,7 @@
 from tkinter import * 
 from tkinter import messagebox
 from tkinter import ttk
-from db_produto import registrar_produto
+from db_produto import registrar_produto, atualizar_produto, pesquisar_produto
 
 # Criando classe principal, que carrega a janela e tudo o que há nela
 class crud_produtos:
@@ -22,52 +22,55 @@ class crud_produtos:
         frame_botoes.grid(row=1)
 
         # Labels vazios para divisoes
-        margem_de_cima = Label(frame_botoes, text="", height=2).grid(row=1, column=1)
-        margem_da_esquerda = Label(frame_botoes, text="", width=10).grid(row=1, column=1)
-        divisao1 = Label(frame_botoes, text="", width=7).grid(row=3, column=3)
-        divisao2 = Label(frame_botoes, text="", width=7).grid(row=5, column=5)
-        divisao3 = Label(frame_botoes, text="", width=7).grid(row=7, column=7)
+        Label(frame_botoes, text="", height=2).grid(row=1, column=1)
+        Label(frame_botoes, text="", width=10).grid(row=1, column=1)
+        Label(frame_botoes, text="", width=7).grid(row=3, column=3)
+        Label(frame_botoes, text="", width=7).grid(row=5, column=5)
+        Label(frame_botoes, text="", width=7).grid(row=7, column=7)
         
-        # Botoes necessarios
-        botao_menu_cadastrar = Button(frame_botoes, text="Cadastrar produto", command=self.menu_cadastrar, width=18, height=1).grid(row=2, column=2)
-        botao_menu_alterar = Button(frame_botoes, text="Alterar produto", width=18, height=1).grid(row=2, column=4)
-        botao_menu_deletar = Button(frame_botoes, text="Deletar produto", width=18, height=1).grid(row=2, column=6)
-        botao_menu_pesquisar = Button(frame_botoes, text="Pesquisar produto", width=18, height=1).grid(row=2, column=8)
+        # Botoes que ficam na parte de cima do layout, carrega as funcoes
+        Button(frame_botoes, text="Cadastrar produto", command=self.registrar_no_banco, width=18, height=1).grid(row=2, column=2)
+        Button(frame_botoes, text="Alterar produto", width=18, height=1).grid(row=2, column=4)
+        Button(frame_botoes, text="Deletar produto", width=18, height=1).grid(row=2, column=6)
+        Button(frame_botoes, text="Pesquisar produto", command=self.pesquisar_no_banco, width=18, height=1).grid(row=2, column=8)
         
-    def menu_cadastrar(self):
         # Criando frame que carrega itens de cadastro
         frame_cadastrar = Frame(self.root, width=900, height=500)
         frame_cadastrar.grid(row=2)
 
         # Labels vazios para divisoes
-        divisao = Label(frame_cadastrar, text="", height=1).grid(row=2)
-        divisao = Label(frame_cadastrar, text="", height=1).grid(row=4)
-        divisao = Label(frame_cadastrar, text="", height=1).grid(row=6)
-        divisao = Label(frame_cadastrar, text="", height=2).grid(row=8)
-        divisao = Label(frame_cadastrar, text="", height=1).grid(row=10)
+        Label(frame_cadastrar, text="", height=1).grid(row=2)
+        Label(frame_cadastrar, text="", height=1).grid(row=4)
+        Label(frame_cadastrar, text="", height=1).grid(row=6)
+        Label(frame_cadastrar, text="", height=2).grid(row=8)
+        Label(frame_cadastrar, text="", height=1).grid(row=10)
 
         # Label e entry para 'nome' do produto
-        label_nome = Label(frame_cadastrar, text="Nome do Produto:").grid(row=1, column=1)
-        self.box_nome = Entry(frame_cadastrar, width=25).grid(row=1, column=3)
+        Label(frame_cadastrar, text="Nome do Produto:").grid(row=1, column=1)
+        self.box_nome = Entry(frame_cadastrar, width=25)
+        self.box_nome.grid(row=1, column=3)
 
         # Label e entry para 'descricao' do produto
-        label_descricao = Label(frame_cadastrar, text="Descrição do Produto:").grid(row=3, column=1)
-        self.box_descricao = Entry(frame_cadastrar, width=25).grid(row=3, column=3)
+        Label(frame_cadastrar, text="Descrição do Produto:").grid(row=3, column=1)
+        self.box_descricao = Entry(frame_cadastrar, width=25)
+        self.box_descricao.grid(row=3, column=3)
 
         # Label e entry para 'quantidade' do produto
-        label_quantidade = Label(frame_cadastrar, text="Quantidade do Produto:").grid(row=5, column=1)
-        self.box_quantidade = Entry(frame_cadastrar, width=25).grid(row=5, column=3)
-        
-        # Label e entry para 'valor' do produto
-        label_valor = Label(frame_cadastrar, text="Valor do Produto:").grid(row=7, column=1)
-        self.box_valor = Entry(frame_cadastrar, width=25).grid(row=7, column=3)
+        Label(frame_cadastrar, text="Quantidade do Produto:").grid(row=5, column=1)
+        self.box_quantidade = Entry(frame_cadastrar, width=25)
+        self.box_quantidade.grid(row=5, column=3)
 
-        # Botoes 'registrar' e 'voltar'
-        botao_registrar_produto = Button(frame_cadastrar, text="REGISTRAR", command=self.registrar_no_banco, width=12, height=1).grid(row=9, column=2)
-        botao_voltar = Button(frame_cadastrar, text="VOLTAR", width=12, height=1).grid(row=11, column=2)
+        # Label e entry para 'valor' do produto
+        Label(frame_cadastrar, text="Valor do Produto:").grid(row=7, column=1)
+        self.box_valor = Entry(frame_cadastrar, width=25)
+        self.box_valor.grid(row=7, column=3)
+
+        # Text area usado para retornar dados ja existentes
+        self.text_area = Text(frame_cadastrar,height=10,width=80)
+        self.text_area.grid(row=10,column=0,columnspan=4)
 
     def registrar_no_banco(self):
-        nome_produto = self.box_nome.get()
+        nome_produto = self.box_nome.get().title()
         descricao_produto = self.box_descricao.get()
         quantidade_produto = self.box_quantidade.get()
         valor_produto = self.box_valor.get()
@@ -75,14 +78,40 @@ class crud_produtos:
         if nome_produto and descricao_produto and quantidade_produto and valor_produto:
             registrar_produto(nome_produto, descricao_produto, quantidade_produto, valor_produto)
 
-            self.box_nome.delete(0, Tk.END)
-            self.box_descricao.delete(0, Tk.END)
-            self.box_quantidade.delete(0, Tk.END)
-            self.box_valor.delete(0, Tk.END)
+            self.box_nome.delete(0, END)
+            self.box_descricao.delete(0, END)
+            self.box_quantidade.delete(0, END)
+            self.box_valor.delete(0, END)
 
             messagebox.showinfo("Sucesso", "Produto cadastrado com sucesso!")
         else:
             messagebox.showerror("Error", "Todos os campos são obrigatorios")
+
+    def alterar_no_Banco(self):
+        nome_produto = self.box_nome.get().title()
+        descricao_produto = self.box_descricao.get()
+        quantidade_produto = self.box_quantidade.get()
+        valor_produto = self.box_valor.get()
+
+        if nome_produto and descricao_produto and quantidade_produto and valor_produto:
+            atualizar_produto(nome_produto, descricao_produto, quantidade_produto, valor_produto)
+
+            self.box_nome.delete(0, END)
+            self.box_descricao.delete(0, END)
+            self.box_quantidade.delete(0, END)
+            self.box_valor.delete(0, END)
+
+            messagebox.showinfo("Sucesso", "Produto atualizado com sucesso!")
+        else:
+            messagebox.showerror("Error", "Todos os campos são obrigatorios")
+
+    def pesquisar_no_banco(self):
+            try:
+                nome = self.box_nome.get().title()
+                result = pesquisar_produto(nome)
+                self.text_area.insert(END, result)
+            except:
+                messagebox.showerror("Error", "Campo 'Nome' não preenchido ou Produto não encontrado!")
 
 if __name__ == "__main__":
     root = Tk()
