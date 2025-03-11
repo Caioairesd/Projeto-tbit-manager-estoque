7b670c9a9b8d4f0ba3e8e859069a2132bce5b01e
EberthSilva <eberthsilva011@gmail.com>
2025-03-10 22:25:54 -0300

    99% pronto
    .

--- Funcionario/Config.py
+++ Funcionario/Config.py
@@ -1,5 +1,13 @@
 MYSQL_HOST = 'localhost' 
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'root'
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 MYSQL_DATABASE = 'tbit_db'
 #pip install mysql-connector-python
+=======
+MYSQL_DATABASE = 'tbit_db'
+>>>>>>> Stashed changes
+=======
+MYSQL_DATABASE = 'tbit_db'
+>>>>>>> Stashed changes
