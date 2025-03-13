5354ac88607b8d238228049a3c0933d27c351ddf
EberthSilva <eberthsilva011@gmail.com>
2025-03-12 22:05:50 -0300

    conexao pronta
    conexao pronta, criar e excluir funcionais

--- Funcionario/crud_funcionario.py
+++ Funcionario/crud_funcionario.py
@@ -13,39 +13,39 @@ def login_funcionario(nome, Data_de_nascimento,  Data_de_admissao,  CPF, Cidade,
 
     conn = get_connection()
     cursor = conn.cursor()
-    query = "insert INTO funcionario(id_funcionario, nome_funcionario, data_nascimento_funcionario, data_admissao_funcionario ,cpf_funcionario, cidade_funcionario, estado_funcionario , telefone_funcionario, email_funcionario, usuario_funcionario,senha_funcionario) VALUES(%s, %s, %s, %s, %s,%s,%s,%s,%s,%s)"
+    query = "insert funcionario(nome_funcionario, data_nascimento_funcionario, data_admissao_funcionario ,cpf_funcionario, cidade_funcionario, estado_funcionario , telefone_funcionario, email_funcionario, usuario_funcionario,senha_funcionario) VALUES(%s, %s, %s, %s, %s,%s,%s,%s,%s,%s)"
     cursor.execute(query, (nome, Data_de_nascimento, Data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha))
     conn.commit()
     cursor.close()
     conn.close()
 
-def editar_funcionario():
+def buscar_funcionario(id_funcionario):
+
     conn = get_connection()
     cursor = conn.cursor()
-    query = "SELECT * FROM funcionario"
-    cursor.execute(query)
-    result = cursor.fetchall()
+    query = "SELECT * FROM funcionario WHERE id_funcionario = %s"
+    cursor.execute(query, (id_funcionario,))
+    result = cursor.fetchone()
     cursor.close()
     conn.close()
     return result
 
-def excluir_funcionario ():
+def editar_funcionario(nome, Data_de_nascimento, Data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha, funcionario_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "UPDATE funcionario SET nome = %s,  Data_de_nascimento = %s , Data_de_admissao=%s, CPF=%s, Cidade=%s, UF=%s, Telefone=%s, Email=%s, Usuario=%s, Senha=%s WHERE id_funcionario = %s"
+    cursor.execute(query, (nome, Data_de_nascimento, Data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha, funcionario_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+  
+def excluir_funcionario (funcionario_id):
 
     conn = get_connection()
     cursor = conn.cursor()
     query = "DELETE FROM funcionario WHERE id_funcionario = %s"
-    cursor.execute(query, (id,))
+    cursor.execute(query, (funcionario_id,))
     conn.commit()
     cursor.close()
     conn.close()
 
-def buscar_funcionario():
-
-    conn = get_connection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM usuario"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return result
--- Funcionario/tela_funcioario.py
+++ Funcionario/tela_funcioario.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from crud_funcionario import login_funcionario, editar_funcionario, excluir_funcionario, buscar_funcionario
+from crud_funcionario import login_funcionario,excluir_funcionario, editar_funcionario, buscar_funcionario
 from tkinter import *
 
 
@@ -14,20 +14,18 @@ class CRUDfuncionario:
        
         self.root.config(bg="lightgray")
 
-        self.criar_funcionario()
-        self.editar_funcionario()
-        self.excluir_funcionario()
-        self.buscar_funcionario()
+        self.tela_funcionario()
+       
         
        
-    
+ 
+    def tela_funcionario(self):
 
-    def criar_funcionario(self):
         #Labels com o texto e sem fundo
        
         tk.Label(self.root, text="Nome:", bg="lightgray").place(x=30, y=20)
         tk.Label(self.root, text="Data de Nascimento:", bg="lightgray").place(x=30, y=60)
-        tk.Label(self.root, text="Data de admissão:", bg="lightgray").place(x=30, y=100)
+        tk.Label(self.root, text="Data de admissão:",  bg="lightgray").place(x=30, y=100)
         tk.Label(self.root, text="CPF:", bg="lightgray").place(x=30, y=140)
         tk.Label(self.root, text="Cidade:", bg="lightgray").place(x=30, y=180)
         tk.Label(self.root, text="Estado:", bg="lightgray").place(x=30, y=220)
@@ -37,9 +35,11 @@ class CRUDfuncionario:
         tk.Label(self.root, text="Senha:", bg="lightgray").place(x=30, y=380)
         
 
+        
+
         # Caixas cinza escuro 
         
-        self.nome_entry = tk.Entry(self.root, bg="darkgray")
+        self.nome_entry  = tk.Entry(self.root, bg="darkgray")
         self.Data_de_nascimento_entry = tk.Entry(self.root, bg="darkgray")
         self.Data_de_Admissao_entry = tk.Entry(self.root, bg="darkgray")
         self.CPF_entry = tk.Entry(self.root, bg="darkgray")
@@ -49,10 +49,10 @@ class CRUDfuncionario:
         self.Email_entry = tk.Entry(self.root, bg="darkgray")
         self.Usuario_entry = tk.Entry(self.root, bg="darkgray")
         self.Senha_entry = tk.Entry(self.root, bg="darkgray", show="*") 
-        
-        
+
+
         #caixas de texto
-        
+
         self.nome_entry.place(x=180, y=20, width=250)
         self.Data_de_nascimento_entry.place(x=180, y=60, width=250)
         self.Data_de_Admissao_entry.place(x=180, y=100, width=250)
@@ -63,13 +63,20 @@ class CRUDfuncionario:
         self.Email_entry.place(x=180, y=300, width=250)
         self.Usuario_entry.place(x=180, y=340, width=250)
         self.Senha_entry.place(x=180, y=380, width=250)
-        
 
-        # Botão de login
-        tk.Button(self.root, text="FAZER LOGIN", command=self.login_funcionario).place(x=30, y=460)
+
+        tk.Label(self.root, text="ID:", bg="lightgray").place(x=290, y=463)
+        self.funcionario_id_entry = tk.Entry(self.root, bg="darkgray")
+        self.funcionario_id_entry.place(x=310, y=465, width=70 )
+
+        #botôes de funcões
+        tk.Button(self.root, text="CRIAR", command=self.login_funcionario).place(x=30, y=460)
+        tk.Button(self.root, text="EXCLUIR", command=self.excluir_funcionario).place(x=90, y=460)
+        tk.Button(self.root, text="EDITAR", command=self.editar_funcionario).place(x=160, y=460)
+        tk.Button(self.root, text="BUSCAR", command=self.buscar_funcionario).place(x=225, y=460)
 
     def login_funcionario(self):
-        
+
         nome = self.nome_entry.get()
         Data_de_nascimento = self.Data_de_nascimento_entry.get()
         Data_de_admissao = self.Data_de_Admissao_entry.get()
@@ -102,8 +109,9 @@ class CRUDfuncionario:
 
         
     def editar_funcionario(self):
-        tk.Button(self.root, text="EDITAR", command=self.editar_funcionario).place(x=120, y=460)
-        user_id = self.id_entry.get()
+
+        
+        funcionario_id = self.funcionario_id_entry.get()
         nome = self.nome_entry.get()
         nascimento = self.Data_de_nascimento_entry.get()    
         admissao = self.Data_de_Admissao_entry.get()  
@@ -115,8 +123,8 @@ class CRUDfuncionario:
         usuario = self.Usuario_entry.get()    
         senha = self.Senha_entry.get()  
 
-        if user_id and nome and nascimento and admissao and CPF and cidade and UF and telefone and Email  and usuario and  senha:
-            editar_funcionario (nome, nascimento,admissao, CPF, cidade, UF, telefone, Email, usuario, senha, user_id)
+        if funcionario_id and nome and nascimento and admissao and CPF and cidade and UF and telefone and Email  and usuario and  senha:
+            editar_funcionario (nome, nascimento,admissao, CPF, cidade, UF, telefone, Email, usuario, senha, funcionario_id)
             self.nome_entry.delete(0,tk.END)
             self.Data_de_nascimento_entry.delete(0,tk.END)
             self.Data_de_Admissao_entry.delete(0,tk.END)
@@ -126,39 +134,40 @@ class CRUDfuncionario:
             self.Telefone_entry.delete(0,tk.END)
             self.Email_entry.delete(0,tk.END)
             self.Senha_entry.delete(0,tk.END)
-            self.id_entry.delete(0,tk.END)
+            self.funcionario_id_entry.delete(0,tk.END)
 
-            messagebox.showinfo("Sucess", "usuario atualizado com sucesso")
+            messagebox.showinfo("Sucess", "Funcionario atualizado com sucesso")
         else:
-            messagebox.showerror("Error", "Todos os campos são obrigatorios")
+            messagebox.showerror("Error", "ID do funcioonario é obrigatorio")
         
 
     def excluir_funcionario(self):
-        tk.Button(self.root, text="EXCLUIR", command=self.excluir_funcionario).place(x=180, y=460)
-
-
-        self.text_area = tk.Text(self.root, height = 10, width=80)
+        funcioanrio_id = self.funcionario_id_entry.get()
+        if funcioanrio_id:
+            excluir_funcionario(funcioanrio_id)
+            self.funcionario_id_entry.delete(0,tk.END)
+            messagebox.showinfo ("Sucess", "Funcionario excluido com sucesso!")
+        else:
+            messagebox.showerror ("Error", "ID do funcioonario é obrigatorio")
     def buscar_funcionario (self):
-        tk.Button(self.root, text="BUSCAR", command=self.excluir_funcionario).place(x=245, y=460)
-        tk.Label(self.root, text="ID:", bg="lightgray").place(x=310, y=463)
-        self.id_entry = tk.Entry(self.root, bg="darkgray")
-        self.id_entry.place(x=330, y=465, width=50)
-
-        funcionarios = buscar_funcionario()
+        self.text_area = tk.Text(self.root, height = 10, width=100,  bg="lightgray")
+        self.text_area.place(x= 30, y= 500 )
+        id = self.funcionario_id_entry.get()
+        funcionario = buscar_funcionario(id)
         self.text_area.delete(1.0, tk.END)   
-        for user in funcionarios:
-            self.text_area.insert(tk.END,f"ID:{user [0]}, Nome: {user [1]}, telefone: {user[2]},email:{user[3]}\n"  ) 
-       
-    
+        self.text_area.insert(tk.END,f"ID:{funcionario [0]}, nome: {funcionario [1]}, Data_de_nascimento: {funcionario[2]},Data_de_admissao:{funcionario[3]}, CPF:{funcionario[4]}, Cidade:{funcionario[5]}, UF:{funcionario[6]}, Telefone:{funcionario[7]}, Email:{funcionario[8]}, Usuario: {funcionario[9]}\n"  ) 
 
-
-
-
-
-
-
-
-
+    def limpar_campos(self):
+        self.nome_entry.delete(0,tk.END)
+        self.Data_de_nascimento_entry.delete(0,tk.END)
+        self.Data_de_Admissao_entry.delete(0,tk.END) 
+        self.CPF_entry.delete(0,tk.END) 
+        self.Cidade_entry.delete(0,tk.END)
+        self.UF_entry.delete(0,tk.END) 
+        self.Telefone_entry.delete(0,tk.END) 
+        self.Email_entry.delete(0,tk.END) 
+        self.Usuario_entry.delete(0,tk.END) 
+        self.Senha_entry.delete(0,tk.END) 
 if __name__ == "__main__":
     root = tk.Tk()
     app = CRUDfuncionario(root)
