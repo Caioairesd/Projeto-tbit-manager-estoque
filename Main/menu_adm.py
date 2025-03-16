2a1bd16243def9e4ecc4b7e9675d310b717f8742
baleradaniel <danielbalera021@gmail.com>
2025-03-15 21:01:40 -0300

    tela funcionario quase funcional

--- Main/menu_adm.py
+++ Main/menu_adm.py
@@ -1,25 +1,25 @@
-import tkinter as tk
+import tkinter as tk 
 from tkinter import *
 from tela_fornecedor_admin import tela_fornecedor_admin
 from tela_produto_admin import tela_produto_admin
-from tela_funcioario import tela_funcionario
+from tela_funcionario import tela_funcionario
 
 class menu_admin:
-    def __init__(self,root):
+    def __init__(self, root):
         self.root = root
         self.root.title("TBit Manager by TerraBytes - Menu Principal - Administrador") 
         self.create_widget()
         self.root.geometry("700x500")
-        self.root.resizable( width = False, height = False)
+        self.root.resizable(width=False, height=False)
 
     def create_widget(self):
-        registrar_button = tk.Button(text='Registrar Funcionario', width=20, command= tela_funcionario)
+        registrar_button = tk.Button(self.root, text='Registrar Funcionario', width=20, command=self.button_tela_funcionario)
         registrar_button.place(x=125, y=80)
 
-        fornecedor_button = tk.Button(text='Fornecedores', width=20, command=self.button_tela_fornecedor_admin)
+        fornecedor_button = tk.Button(self.root, text='Fornecedores', width=20, command=self.button_tela_fornecedor_admin)
         fornecedor_button.place(x=275, y=80)
 
-        produto_button = tk.Button(text='Produtos', width=20, command=self.button_tela_produto_admin)
+        produto_button = tk.Button(self.root, text='Produtos', width=20, command=self.button_tela_produto_admin)
         produto_button.place(x=425, y=80)
 
     # CLASSES CRIADAS PARA PASSAR O ROOT AS CLASSES QUE IMPORTAMOS
