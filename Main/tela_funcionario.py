9ddfa765624c9521e54e26a7aa6af8e38646fc02
baleradaniel <danielbalera021@gmail.com>
2025-03-17 00:49:06 -0300

    tela funcionario

--- Main/tela_funcionario.py
+++ Main/tela_funcionario.py
@@ -1,23 +1,26 @@
 import tkinter as tk
 from tkinter import messagebox
-from database_geral import registrar_funcionario
+from database_geral import register_funcionario, delete_funcionario, update_funcionario, pesquisar_funcionario, listar_funcionarios_crud
 from tkinter import*
 from menu_adm import *
 
 
 class tela_funcionario:
     def __init__(self, root):
-        self.root = root
-        self.root.title("CRUD FUNCIONARIO                                                                                                                                                                       TerraBytes")
+        self.root = tk.Toplevel(root) 
+        self.root.title("CRUD FUNCIONARIO TerraBytes")
         self.root.geometry("900x700")
         self.root.resizable(width=False, height=False)
+
+        self.root.transient(root)  # Mantém a janela no topo
+        self.root.grab_set()  # Bloqueia interações na principal até fechar essa
 
         self.root.config(bg="lightgray")
         self.tela_funcionario()
 
         self.listar_funcionarios()
 
-    def tela_funcionario(self):
+    def create_widgets(self):
         # Labels com o texto e sem fundo
         tk.Label(self.root, text="Nome:", bg="lightgray").place(x=30, y=20)
         tk.Label(self.root, text="Data de Nascimento:", bg="lightgray").place(x=30, y=60)
@@ -64,15 +67,15 @@ class tela_funcionario:
 
         # Botões de funções
         tk.Button(self.root, text="CANCELAR", command=self.cancelar).place(x=450, y=420)
-        tk.Button(self.root, text="CRIAR", command=self.login_funcionario).place(x=30, y=420)
-        tk.Button(self.root, text="EXCLUIR", command=self.excluir_funcionario).place(x=90, y=420)
-        tk.Button(self.root, text="EDITAR", command=self.editar_funcionario).place(x=160, y=420)
-        tk.Button(self.root, text="BUSCAR", command=self.buscar_funcionario).place(x=225, y=420)
+        tk.Button(self.root, text="CRIAR", command=self.register_funcionario).place(x=30, y=420)
+        tk.Button(self.root, text="EXCLUIR", command=self.delete_funcionario).place(x=90, y=420)
+        tk.Button(self.root, text="EDITAR", command=self.update_funcionario).place(x=160, y=420)
+        tk.Button(self.root, text="BUSCAR", command=self.pesquisar_funcionario).place(x=225, y=420)
 
         self.text_area = tk.Text(self.root, height=12, width=105, bg="lightgray")
         self.text_area.place(x=30, y=480)
 
-    def login_funcionario(self):
+    def register_funcionario(self):
         nome = self.nome_entry.get()
         Data_de_nascimento = self.Data_de_nascimento_entry.get()
         Data_de_admissao = self.Data_de_Admissao_entry.get()
@@ -88,7 +91,7 @@ class tela_funcionario:
         data_de_admissao = self.inverter_data(Data_de_admissao)
 
         if nome and data_de_nascimento and data_de_admissao and CPF and Cidade and UF and Telefone and Email and Usuario and Senha:
-            login_funcionario(nome, data_de_nascimento, data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha)
+            self.register_funcionario(nome, data_de_nascimento, data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha)
             self.limpar_campos()
             messagebox.showinfo("Success", "Funcionario criado com Sucesso")
         else:
@@ -100,12 +103,12 @@ class tela_funcionario:
         for funcionario in funcionarios:
             self.text_area.insert(tk.END, f"id:{funcionario[0]}, Nome: {funcionario[1]}, Data de Nascimento: {funcionario[2]}, Data de Admissão: {funcionario[3]}, CPF: {funcionario[4]}, Cidade: {funcionario[5]}, UF: {funcionario[6]}, Telefone: {funcionario[7]}, Email: {funcionario[8]}, Usuario: {funcionario[9]}, Senha: {funcionario[10]}\n\n")
 
-    def buscar_funcionario(self):
+    def pesquisar_funcionario(self):
         busca = self.buscar_entry.get()
         self.buscar_entry.delete(0, tk.END)
 
         if busca:
-            id_solicitado = buscar_funcionario(busca)
+            id_solicitado = pesquisar_funcionario(busca)
 
             if id_solicitado:
                 messagebox.showinfo("Sucesso", f"Funcionário {id_solicitado[1]} encontrado com sucesso!")
@@ -133,7 +136,7 @@ class tela_funcionario:
         else:
             messagebox.showerror("Erro", "Campo de busca vazio")
 
-    def editar_funcionario(self):
+    def update_funcionario(self):
         funcionario_id = self.funcionario_id_entry.get()
         nome = self.nome_entry.get()
         Data_de_nascimento = self.Data_de_nascimento_entry.get()
@@ -147,19 +150,19 @@ class tela_funcionario:
         Senha = self.Senha_entry.get()
 
         if funcionario_id and nome and Data_de_nascimento and Data_de_admissao and CPF and Cidade and UF and Telefone and Email and Usuario and Senha:
-            editar_funcionario(nome, Data_de_nascimento, Data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha, funcionario_id)
+            update_funcionario(nome, Data_de_nascimento, Data_de_admissao, CPF, Cidade, UF, Telefone, Email, Usuario, Senha, funcionario_id)
             messagebox.showinfo("Sucesso", "Funcionário atualizado com sucesso")
             self.limpar_campos()
             self.listar_funcionarios()
         else:
             messagebox.showerror("Erro", "Todos os campos são obrigatórios")
 
-    def excluir_funcionario(self):
+    def delete_funcionario(self):
         funcionario_id = self.funcionario_id_entry.get()
         confirmacao = messagebox.askyesno("Confirmação", "Você realmente deseja deletar esse funcionário?")
         if confirmacao:
             if funcionario_id:
-                excluir_funcionario(funcionario_id)
+                delete_funcionario(funcionario_id)
                 self.funcionario_id_entry.delete(0, tk.END)
                 self.listar_funcionarios()
                 messagebox.showinfo("Sucesso", "Funcionário deletado com sucesso!")
@@ -195,6 +198,8 @@ class tela_funcionario:
         print(data_banco)
 
         return data_banco
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
