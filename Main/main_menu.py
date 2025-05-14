039e8760189693ecad6c1acd5984c9aa4fc48f94
EberthSilva <eberthsilva011@gmail.com>
2025-05-13 22:10:30 -0300

    adm login pronto
    - menu  adm login: ok
    - menu usuário login : em andamento

--- Main/main_menu.py
+++ Main/main_menu.py
@@ -10,34 +10,47 @@ class login_menu:
         tbit_db.__init__(self)
         
         self.root = root
-        self.root.title("TBit Manager by TerraBytes") 
+        self.root.title("TBit Manager by TerraBytes")
+        self.root.geometry('600x400')
+    
+        
+        ctk.set_appearance_mode("dark")
+        
+
         self.create_widget()
-        self.root.geometry("600x300")
+        #self.root.geometry(f"{largura}x{altura}+0+0") 
+
         self.root.resizable( width = False, height = False)
         pass
 
     def create_widget(self):
-        self.left_frame = ctk.CTkFrame(self.root, width=200, height=300, fg_color="Black") # Cria um frame à esquerda
-        self.left_frame.pack(side=LEFT) # Posiciona o frame à esquerda
+      
 
-        self.right_frame = ctk.CTkFrame(self.root, width=400, height=300, fg_color="Black") # Cria um frame à esquerda
-        self.right_frame.pack(side=RIGHT) # Posiciona o frame à esquerda
+        self.right_frame = ctk.CTkFrame(self.root, width=400, height=300, fg_color="gray")# definir o tamanho e cor do fundo da frame
+        self.right_frame.pack(padx=0, pady=0, expand=True, fill="both")# definir a expanção da frame
 
+        #self.right_frame.pack(side=RIGHT) # Posiciona o frame à esquerda
 
-        self.usuario_label = ctk.CTkLabel(self.right_frame, text="Usuario:", font=("Times New Roman", 20), fg_color="Black", text_color='white') # Cria um label para o usuario
-        self.usuario_label.place(x=10, y=105) # Posiciona o label o frame direito
-
-        self.usuario_entry = ctk.CTkEntry(self.right_frame, width=140,height=30) # Cria um campo de entrada para o usuario
-        self.usuario_entry.place(x=100, y=100) # Posiciona o campo de entrada
-
-        self.senha_label = ctk.CTkLabel(self.right_frame, text="Senha:", font=("", 20), fg_color="Black", text_color="White") # Cria um label para a senha
-        self.senha_label.place(x=10, y=160) # Posiciona o label no frame direito
-
-        self.senha_entry = ctk.CTkEntry(self.right_frame, width=140,height=30, show="*") # Cria um campo de entrada para a senha
-        self.senha_entry.place(x=100, y=160)       
+        self.usuario_label = ctk.CTkLabel(self.right_frame, text="B e m    V i n d o",font=("Garamond", 30), fg_color="gray", text_color='black') # Cria um label para o usuario
+        self.usuario_label.place(x=210, y=50) # Posiciona o label o frame direito
         
-        self.login_button = ctk.CTkButton(self.right_frame, text="LOGIN", width=15, command=self.login_user) # Cria um botao de login
-        self.login_button.place(x=80, y=225)
+
+
+
+        self.usuario_label = ctk.CTkLabel(self.right_frame, text="Usuario:", font=("Times New Roman", 20), fg_color="gray", text_color='black') # Cria um label para o usuario
+        self.usuario_label.place(x=150, y=140) # Posiciona o label o frame direito
+
+        self.usuario_entry = ctk.CTkEntry(self.right_frame, width=140,height=30, fg_color='lightgray') # Cria um campo de entrada para o usuario
+        self.usuario_entry.place(x=225, y=140) # Posiciona o campo de entrada
+
+        self.senha_label = ctk.CTkLabel(self.right_frame, text="Senha:", font=("Times New Roman", 20), fg_color="gray", text_color="black") # Cria um label para a senha
+        self.senha_label.place(x=160, y=190) # Posiciona o label no frame direito
+
+        self.senha_entry = ctk.CTkEntry(self.right_frame, width=140,height=30, fg_color='lightgray',show="*") # Cria um campo de entrada para a senha
+        self.senha_entry.place(x=225, y=190)       
+        
+        self.login_button = ctk.CTkButton(self.right_frame, text="LOGIN",text_color='black' ,width=15,fg_color='darkgray', command=self.login_user) # Cria um botao de login
+        self.login_button.place(x=225, y=240)
 
     def login_user(self):
         
--- Main/menu_user.py
+++ Main/menu_user.py
@@ -5,34 +5,38 @@ from tela_pedido import tela_pedido
 from tela_reabastecimento import tela_reabastecimento
 from tela_estoque import tela_estoque
 
-
-
 class menu_usuario:
     def __init__(self,root):
     
         self.root = root
-        self.root.title("TBit Manager by TerraBytes - Menu Principal - Usuario") 
+        self.root.title(" Menu Principal - Usuario") 
         #self.create_widget()
-
-        self.root.geometry("900x700")
-        #self.root.config(fg_color="black")
+        self.root.configure(fg_color="gray")
+        self.root.geometry("600x400")
         self.root.resizable( width = False, height = False)
+
+        #self.create_widget()
+        #ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")  # ou "green", "dark-blue"
 
         self.create_widgets()
 
-    def create_widgets(self):        
+    def create_widgets(self):  
+        
+
+        
         #Criação de botões
-        btn_pedido_menu = ctk.CTkButton(self.root,text="Pedido",width=15,height=2,command=self.abrir_tela_pedido)
-        btn_reabastecimento = ctk.CTkButton(self.root,text="Reabastecimento",width=15,height=2,command=self.abrir_tela_reabastecimento)
-        btn_estoque = ctk.CTkButton(self.root,text="Estoque",width=15,height=2,command=self.abrir_tela_estoque)
-        btn_logout = ctk.CTkButton(self.root, text='Logout', width=18, command=self.logout_usuario)
-            
+        btn_pedido_menu = ctk.CTkButton(self.root,text="Pedido",text_color= 'black',width=80,fg_color= '#D3D3D3',height=35,command=self.abrir_tela_pedido)
+        btn_reabastecimento = ctk.CTkButton(self.root,text="Reabastecimento",text_color= 'black',width=80,fg_color= '#D3D3D3',height=35,command=self.abrir_tela_reabastecimento)
+        btn_estoque = ctk.CTkButton(self.root,text="Estoque",text_color= 'black',width=80,height=35,fg_color= '#D3D3D3',command=self.abrir_tela_estoque)
+        btn_logout = ctk.CTkButton(self.root, text='Logout', text_color= 'black',width=80,height=35,fg_color= 'darkgray',command=self.logout_usuario)
         pass
             
-        btn_pedido_menu.place(x=275, y=120)
-        btn_reabastecimento.place(x=275, y=160)
-        btn_estoque.place(x=275, y=200)
-        btn_logout.place(x=600, y=400)
+
+        btn_pedido_menu.place(x=100, y=120)
+        btn_reabastecimento.place(x=100, y=160)
+        btn_estoque.place(x=100, y=200)
+        btn_logout.place(x=500, y=345)
     
     
     def abrir_tela_pedido(self):
