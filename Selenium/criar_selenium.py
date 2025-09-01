6d768a12c2abe87e7e6ee0115479d7bd6f583770
mazzaKaio <mazza.kaio12@gmail.com>
2025-09-01 19:29:40 -0300

    Criando função para testar com selenium, e uma função para inverter o padrão da data

--- Selenium/criar_selenium.py
+++ Selenium/criar_selenium.py
@@ -3,24 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Entradas que vão ser testadas:
-nome_completo = "Caio Aires"
-data_nascimento = "30/01/2008"
-data_admissao = "17/02/2025"
-cpf = "000.000.000-00"
-cidade = "Joinville"
-uf = "SC"
-telefone = "4002-8922"
-email = "caio@gmail.com"
-usuario = "caio_aires"
-senha = "1234"
-perfil = "ADM"
+# Função para teste automatizado
+def selenium_test(driver, nome, data_n, data_a, cpf, cidade, uf, telefone, email, usuario, senha, perfil):
 
-# Inicia o servicção do ChromeDriver
-servico = webdriver.ChromeService(executable_path="C:/xampp/htdocs/Projeto-tbit-manager-estoque/chromedriver-win64/chromedriver.exe")
-driver = webdriver.Chrome(service=servico)
-
-try:
     # 1. Abre a página do app de estoque
     driver.get("http://localhost:8080/Projeto-tbit-manager-estoque/Selenium/criar.php")
 
@@ -41,9 +26,9 @@ try:
 
     botao_cadastrar = driver.find_element(By.ID, "cadastrar_usuario")
 
-    campo_nome.send_keys(nome_completo)
-    campo_data_n.send_keys(data_nascimento)
-    campo_data_a.send_keys(data_admissao)
+    campo_nome.send_keys(nome)
+    campo_data_n.send_keys(data_n)
+    campo_data_a.send_keys(data_a)
     campo_cpf.send_keys(cpf)
     campo_cidade.send_keys(cidade)
     campo_uf.send_keys(uf)
@@ -58,26 +43,52 @@ try:
     botao_cadastrar.click()
     time.sleep(2) # Pausa para a pagina atualizar
 
-    # 4. Validação: Verifica se o novo produto existe na tabela
-
-    # 5. Acessa a página onde a tabela está
+    # 4. Acessa a página onde a tabela está
     driver.get("http://localhost:8080/Projeto-tbit-manager-estoque/Selenium/listar.php")
 
-    # 6. Espera a tabela aparecer
+    # 5. Espera a tabela aparecer
     time.sleep(3)
 
-    # 7. Verifica se os dados do funcionário aparecem
+    data_n_invert = inverter_data(data_n)
+    data_a_invert = inverter_data(data_a)
+
+    # 6. Verifica se os dados do funcionário aparecem
     tabela = driver.find_element(By.ID, "tabela_funcionarios")
     dados_esperados = [
-        "Caio Aires", "2008-01-30", "2025-02-17", "000.000.000-00",
-        "Joinville", "SC", "4002-8922", "caio@gmail.com",
-        "caio_aires", "1234", "ADM"
+        nome, data_n_invert, data_a_invert, cpf,
+        cidade, uf, telefone, email,
+        usuario, senha, perfil
     ]
 
     if all(dado in tabela.text for dado in dados_esperados):
-        print("Teste de cadastro de funcionário: SUCESSO!")
+        return print("Teste de cadastro de funcionário: SUCESSO!")
     else:
-        print("Teste de cadastro de funcionário: FALHA!")
+        return print("Teste de cadastro de funcionário: FALHA!")
+
+# Função para inverter datas
+def inverter_data(data):
+    test_date = data.split("-")
+    return test_date[2]+"-"+test_date[1]+"-"+test_date[0]
+
+# Entradas que vão ser testadas:
+nome_completo = "Caio Aires"
+data_nascimento = "30-01-2008"
+data_admissao = "17-02-2025"
+cpf = "000.000.000-00"
+cidade = "Joinville"
+uf = "SC"
+telefone = "4002-8922"
+email = "caio@gmail.com"
+usuario = "caio_aires"
+senha = "1234"
+perfil = "ADM"
+
+# Inicia o servicção do ChromeDriver
+servico = webdriver.ChromeService(executable_path="C:/xampp/htdocs/Projeto-tbit-manager-estoque/chromedriver-win64/chromedriver.exe")
+driver = webdriver.Chrome(service=servico)
+
+try:
+    selenium_test(driver, nome_completo, data_nascimento, data_admissao, cpf, cidade, uf, telefone, email, usuario, senha, perfil)
 
 finally:
     driver.quit()
